c81cf833c006cea29459fdf43a59d4f29fca0d5e
Franco Avanzatti <avanzatti5@gmail.com>
2025-03-29 16:57:04 -0300

    backend-co

---
+++ src/config/passport.config.js
@@ -0,0 +1,30 @@
+import passport from 'passport';
+import { Strategy as JwtStrategy, ExtractJwt } from 'passport-jwt';
+import User from '../models/User.model.js';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+// Opciones de configuración de la estrategia JWT
+const opts = {
+  jwtFromRequest: ExtractJwt.fromExtractors([(req) => req.cookies.jwt]), // Obtención desde la cookie 'jwt'
+  secretOrKey: process.env.JWT_SECRET, // Clave secreta para la verificación del token
+};
+
+// Función que inicializa la estrategia Passport
+export default function initializePassport() {
+  passport.use(
+    new JwtStrategy(opts, async (jwt_payload, done) => {
+      try {
+        // Buscar usuario por el id del JWT
+        const user = await User.findById(jwt_payload.id);
+        if (user) {
+          return done(null, user); // El usuario existe y se pasa a la siguiente parte del flujo
+        }
+        return done(null, false); // Si no se encuentra el usuario, fallar el proceso
+      } catch (error) {
+        return done(error, false); // Error al intentar encontrar el usuario
+      }
+    })
+  );
+}
